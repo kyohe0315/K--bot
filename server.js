c1f1cde43b8ad47e57f3f97a8c766f565208d6df
kyohe0315 <ksgn.kyos@gmail.com>
2025-06-13 14:31:00 +0900

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,17 @@ const ICAL_URL = process.env.ICAL_URL;
 const IcalChannelId = process.env.ICAL_CHANNEL_ID;
 const DebackChannelId = process.env.DEBACK_CHANNEL_ID;
 
+const { Client, GatewayIntentBits, Events } = require("discord.js");
 
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildVoiceStates
+  ]
+});
 
 
 client.on('message', async message => {
