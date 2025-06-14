0c0f7a228b5c0c27eab54e718bfce071abf320dc
kyohe0315 <ksgn.kyos@gmail.com>
2025-06-14 11:46:48 +0900

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const vcStartMessages = new Map();
 
 client.once(Events.ClientReady, () => {
   console.log(`${client.user.tag} でログイン中`);
-  client.user.setActivity("第二の人生v2", { type: 0 }); // ← これを追加
+  client.user.setActivity("第二の人生v2.1", { type: 0 }); // ← これを追加
 });
 
 client.on(Events.MessageCreate, async (message) => {
