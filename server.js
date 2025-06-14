1f4d658e37bd9f542ade0b828db371f0523a6476
kyohe0315 <ksgn.kyos@gmail.com>
2025-06-14 14:36:45 +0900

    Update server.js

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ client.on(Events.MessageCreate, async (message) => {
   // ChatGPT呼び出し処理
   if (
     message.mentions.has(client.user) &&
-    message.content.includes("@k-bot.")
+    message.content.includes(@K-bot.#7426 ")
   ) {
     try {
       const prompt = message.content.replace(/<@!?(\d+)>/g, "").trim(); // メンション削除
