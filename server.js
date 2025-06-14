131b9019400832766e343f4d103fee6a07abe7cb
kyohe0315 <ksgn.kyos@gmail.com>
2025-06-14 14:32:08 +0900

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const cron = require("node-cron");
 
 client.once(Events.ClientReady, () => {
   console.log(`${client.user.tag} でログイン中`);
-  client.user.setActivity("第二の人生v2.4", { type: 0 }); // ← これを追加
+  client.user.setActivity("第二の人生v2.5", { type: 0 }); // ← これを追加
   //postMonthlyEvents(); // ← デバッグ用
   //remindCurrentEvents(); // ← デバッグ用← これを追加
 });
