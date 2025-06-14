010d636bf6a0ee5e2ae8c9170348be5529e19141
kyohe0315 <ksgn.kyos@gmail.com>
2025-06-14 13:10:04 +0900

    Update server.js

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ const client = new Client({
 const TOKEN = process.env.DISCORD_BOT_TOKEN;
 const VC_NOTIFY_CHANNEL_ID = process.env.VC_NOTIFY_CHANNEL_ID; // ← 聞き専チャンネル
 const VOICE_CHANNEL_ID = process.env.VOICE_CHANNEL_ID; // ← chatroom1
-const ICAL_CHANNEL_ID = process.env.ICAL_CHANNEL_ID;
-const ICAL_URL = process.env.ICAL_URL;
+const ICAL_CHANNEL_ID = process.env.ICAL_CHANNEL_ID; // ← 星の子の話し場
+const ICAL_URL = process.env.ICAL_URL; // ← GoogleカレンダーURL
 const vcStartMessages = new Map();
 
 client.once(Events.ClientReady, () => {
