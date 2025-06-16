ab8217f8f0f118d07871fe7dface9fc096c1ff12
kyohe0315 <ksgn.kyos@gmail.com>
2025-06-16 09:23:10 +0900

    Update responses.js

--- responses.js
+++ responses.js
@@ -1,16 +1,270 @@
 // responses.js
 module.exports = [
+  
+  // randam ＝ ランダムで1個送信
   {
-    pattern: /こんにちは|やあ|こんちゃ/,
+    pattern: /きょへ。?は/,
+    responses: ["ピカ知識レート2500。元ピカチュウ窓ﾇﾁ!!\nhttps://cdn.discordapp.com/attachments/1022093564995436594/1086669279233069076/kyohe.gif",
+                "sky知識レート2500。skyを始めてから登録者数が爆増し、チャンネル登録者数は驚愕の…！err:情報の取得に失敗しました。⚠人！！",
+                "彼の名は「きょへ。」\n趣味はキャンマラ…ではなくキャンマラのルート構成らしい。\n人間の不利をした歩くバグ。",
+                "現実はポンコツ、ゲームでは中の上。毎日引きこもってます。",
+                "# 👤名前 　： きょへ\n# 🛠️スキル ： 急にVCに現れる変態(？)\n# 🧪特性 　： 突然呪われたかのように表れ、何かの検証を始め、満足して帰っていく。\n# 📝使用上の注意：突然やる気を失うので定期的にけつを叩いて下さい。",
+                "このBotを作り上げた圧倒的天才！多彩にも多彩で出来ない事は基本存在せず…！そのIQはなんと175cmにも及ぶと世間で噂されていたり…いなかったり…。"],
+    type: "random"
+  },
+  { pattern: /AAAB|AABB|ABBB/,
     responses: ["やあ！", "こんにちは～", "元気？", "いやっほー！", "こんにちは！こんばんは！", "ぐへへへへ"],
     type: "random"
   },
-  { pattern: /きょへ。?は/,
-    responses: ["ピカチュウ窓主"],
+  { pattern: /AAAB|AABB|ABBB/,
+    responses: ["やあ！", "こんにちは～", "元気？", "いやっほー！", "こんにちは！こんばんは！", "ぐへへへへ"],
+    type: "random"
+  },
+  { pattern: /AAAB|AABB|ABBB/,
+    responses: ["やあ！", "こんにちは～", "元気？", "いやっほー！", "こんにちは！こんばんは！", "ぐへへへへ"],
+    type: "random"
+  },
+  
+  // static ＝ シンプル処理
+  { pattern: /たっつんは/,
+    responses: ["⠀　　    ゆ\n⠀　  あ  で\n⠀ゆ  あ  た\n⠀で  ゆ  ま\n⠀た  で  ご\n⠀ま  た\n⠀ご  ま\n⠀　  ご",
+                "https://zukan.pokemon.co.jp/zukan-api/up/images/index/596b399e3c5786fc8bf8856adf3f7cf5.png"],
     type: "static"
   },
-  { pattern: /たっつんは/,
-    responses: ["ハゲ"],
+  { pattern: /きょへ。?さんは/,
+    responses: ["きょへさんに何か用かい？メンションすると良いよ。彼は暇人だからね。","え？代わりにメンションしてほしいって？仕方ないなあ～…。あっときょへ@kyohe."],
+    type: "static"
+  },
+  { pattern: /へこにゃんは/,
+    responses: ["百合好き76才　現役JK"],
+    type: "static"
+  },
+  { pattern: /うみは/,
+    responses: ["うみちゅわ～んだよ！！"],
+    type: "static"
+  },
+  { pattern: /たまっぴは/,
+    responses: ["# めんこ～～～いッ‼‼‼","-# あと好奇心旺盛で結構なんでもやる。※ただし失敗は多い"],
+    type: "static"
+  },
+  { pattern: /あっとるは/,
+    responses: ["裏世界探索のプロ!!"],
+    type: "static"
+  },
+  { pattern: /かえは/,
+    responses: ["今となってはワープ系グリッチの先頭に立つ素晴らしい人"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
+    type: "static"
+  },
+  { pattern: /AAA/,
+    responses: ["BBB"],
     type: "static"
   },
   { pattern: /VC開始/,
@@ -18,15 +272,19 @@ module.exports = [
     type: "static"
   },
   {
-    pattern: /テスト/,
+    pattern: /Kテスト/,
     responses: ["これはテストメッセージです。", "すべて正常に動作しています。"],
     type: "static"
   },
+
+  // reverse-delete ＝ 一括表示し、順に削除
   {
     pattern: /うんこ|💩/,
     responses: ["う", "ん", "こ", "だ", "な", "♪"],
     type: "reverse-delete"
   },
+
+  // "progressive ＝ 1つ表示→削除。次を表示→削除
   {
     pattern: /せいは/,
     responses: [
